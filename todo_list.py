8ec6eaac3d32d98e3928a18e8cba01cbabf95156
Héctor Vega <linnk99@gmail.com>
2024-09-11 18:34:36 -0700

    Agregados metodos __init__ y add_task a la clase TodoList
    Bla bla bla bla...

--- todo_list.py
+++ todo_list.py
@@ -12,7 +12,12 @@ class Task:
 
 
 class TodoList:
-    pass
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, description):
+        task = Task(description)
+        self.tasks.append(task)
 
 
 def main():
